1568c1faf910b587898a538ed44c7d3d16b2b0b1
brad <gjrjs31@naver.com>
2025-12-31 21:02:04 +0900

    feat: navigation item 설정
    - title
    - left
    - right

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailScreen from './screens/DetailScreen';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 const Stack = createNativeStackNavigator()
 
@@ -18,8 +19,43 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen 
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "홈",
+            headerStyle: {
+              backgroundColor: '#29b6f6'
+            },
+            headerTintColor: '#ffffff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20
+            }
+          }}
+        />
+        <Stack.Screen 
+          name="Detail"
+          component={DetailScreen}
+          options={{
+            headerBackVisible: false,
+            headerLeft: ({onPress}) => (
+              <TouchableOpacity onPress={onPress}>
+                <Text>Left</Text>
+              </TouchableOpacity>
+            ),
+            headerTitle: ({children}) => (
+              <View>
+                <Text>{children}</Text>
+              </View>
+            ),
+            headerRight: () => (
+              <View>
+                <Text>Right</Text>
+              </View>
+            )
+          }}
+        />
       </Stack.Navigator>    
     </NavigationContainer>
   )
